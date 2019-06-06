dad4f0f026ab8d9f67e2c3b2c7bcbd975831775b
Hyeonho Seo <seohho@gmail.com>
2019-06-06 22:24:11 +0900

    Add '!cyp issue' command & etc..
    - Add '!cyp issue' command
    - Add '!cyp-admin-notify' command
    - Fix #1

--- cypbot.py
+++ cypbot.py
@@ -1,3 +1,4 @@
+import hashlib
 import discord
 from neopleapi import NeopleAPI
 from mdbuilder import MdBuilder
@@ -55,21 +56,43 @@ class CyphersBot(discord.Client, Singleton):
         self.handlertbl = {
                 'help' : [self.handle_helpcmd,
                     '`!cyp help` 디스코드 봇 사용법을 출력합니다'],
+                'issue' : [self.handle_issue,
+                    '`!cyp issue {Message}` 사용중 발견한 버그나 개선사항을 140자 이하로 적어주세요'],
                 'user' : [self.handle_userinfo,
                     '`!cyp user {Username}` 입력한 유저의 기본 정보를 가져옵니다'],
                 'match' : [self.handle_matchlist,
                     '`!cyp match {Username}` 입력한 유저의 최근 전적을 가져옵니다']
         }
+        with open('report_channel.token', 'r') as reportidfile:
+            self.reportchanid = int(reportidfile.readline().strip())
+        print('Report Channel ID is %s' % str(self.reportchanid))
+        with open('notify_md5.token', 'r') as notikeyfile:
+            self.notifykey = notikeyfile.readline().strip()
+        print('Notify Password is %s' % self.notifykey)
 
     async def on_ready(self):
-        print('Logged on as', self.user )
+        print('Logged on as', self.user)
 
     async def on_message(self, message):
         if message.author == self.user:
             return
 
         splited = message.content.split(' ', 2)
-        if splited[0] != '!cyp':
+        
+        if splited[0] == '!cyp-admin-notify':
+            try:
+                notipw = splited[1]
+                notimsg = splited[2]
+                hasher = hashlib.md5(notipw.strip().encode('utf-8'))
+                if hasher.hexdigest() == self.notifykey:
+                    print('Notify sent %s' % notimsg)
+                    await self.send_notify(notimsg)
+                else:
+                    print('Someone tried to send notify %s / %s' % (notipw, notimsg))
+                return
+            except IndexError:
+                return
+        elif splited[0] != '!cyp':
             return
 
         try:
@@ -87,12 +110,30 @@ class CyphersBot(discord.Client, Singleton):
 
         await message.channel.send(await handler[0](cmdargs))
 
+    async def send_notify(self, msg):
+        for channel in self.get_all_channels():
+            try:
+                await channel.send(msg)
+            except AttributeError:
+                pass
+
     async def handle_helpcmd(self, msg):
         helpmsg = '**[도움말]**'
         for cmdidx in list(self.handlertbl.values()):
             helpmsg += '\n'
             helpmsg += cmdidx[1]
         return helpmsg
+
+    async def handle_issue(self, msg):
+        try:
+            limitlen = min(len(msg), 140)  
+            limitmsg = '```' + msg[0:limitlen] + '```'
+            await self.get_channel(self.reportchanid).send(limitmsg)
+        except AttributeError:
+            print('Invalid report channel')
+        except IndexError:
+            print('Index Error Occurred')
+        return '**Thank you for reporting the issue**'
 
     async def handle_userinfo(self, msg):
         rawdata = await CypUser.instance().request(msg)
@@ -104,28 +145,57 @@ class CyphersBot(discord.Client, Singleton):
         userdata = await CypUserInfo.instance().request(None, userid)
         if userdata is None:
             return '**API Error Occurred**'
+        
+        rating_idx = None
+        random_idx = None
 
-        rating_win = UserInfoBuilder.instance().parse(userdata, 'records/$0/winCount', False)
-        rating_lose = UserInfoBuilder.instance().parse(userdata, 'records/$0/loseCount', False)
-        rating_stop = UserInfoBuilder.instance().parse(userdata, 'records/$0/stopCount', False)
-        if rating_lose is not None and rating_stop is not None:
-            rating_lose += rating_stop
+        if UserInfoBuilder.instance().parse(userdata, 'records/$0/gameTypeId') == 'rating':
+            rating_idx = '$0'
+        elif UserInfoBuilder.instance().parse(userdata, 'records/$1/gameTypeId') == 'rating':
+            rating_idx = '$1'
 
-        random_win = UserInfoBuilder.instance().parse(userdata, 'records/$1/winCount', False)
-        random_lose = UserInfoBuilder.instance().parse(userdata, 'records/$1/loseCount', False)
-        random_stop = UserInfoBuilder.instance().parse(userdata, 'records/$1/stopCount', False)
-        if random_lose is not None and random_stop is not None:
-            random_lose += random_stop
+        if UserInfoBuilder.instance().parse(userdata, 'records/$0/gameTypeId') == 'normal':
+            random_idx = '$0'
+        elif UserInfoBuilder.instance().parse(userdata, 'records/$1/gameTypeId') == 'normal':
+            random_idx = '$1'
 
+        if rating_idx is not None:
+            rating_win = UserInfoBuilder.instance().parse(userdata, 'records/' + rating_idx + '/winCount', False)
+            rating_lose = UserInfoBuilder.instance().parse(userdata, 'records/'+ rating_idx + '/loseCount', False)
+            rating_stop = UserInfoBuilder.instance().parse(userdata, 'records/' + rating_idx + '/stopCount', False)
+            try:
+                rating_winrate = round(float(rating_win) / float(rating_win + rating_lose + rating_stop) * 100.0, 2)
+            except ZeroDivisionError:
+                rating_winrate = None
+        else:
+            rating_win = None
+            rating_lose = None
+            rating_stop = None
+            rating_winrate = None
+
+        if random_idx is not None:
+            random_win = UserInfoBuilder.instance().parse(userdata, 'records/' + random_idx + '/winCount', False)
+            random_lose = UserInfoBuilder.instance().parse(userdata, 'records/' + random_idx + '/loseCount', False)
+            random_stop = UserInfoBuilder.instance().parse(userdata, 'records/' + random_idx + '/stopCount', False)
+            try:
+                random_winrate = round(float(random_win) / float(random_win + random_lose + random_stop) * 100.0, 2)
+            except ZeroDivisionError:
+                random_winrate = None
+        else:
+            random_win = None
+            random_lose = None
+            random_stop = None
+            random_winrate = None
+        
         fmtstr = {}
-        try:
-            fmtstr['rating_winrate'] = '%0.2f' % (float(rating_win) / float(rating_win + rating_lose) * 100.0)
-        except:
-            fmtstr['rating_winrate'] = 'Unknown'
-        try:
-            fmtstr['random_winrate'] = '%0.2f' % (float(random_win) / float(random_win + random_lose) * 100.0)
-        except:
-            fmtstr['random_winrate'] = 'Unknown'
+        fmtstr['rating_wincount'] = str(rating_win)
+        fmtstr['rating_losecount'] = str(rating_lose)
+        fmtstr['rating_stopcount'] = str(rating_stop)
+        fmtstr['rating_winrate'] = str(rating_winrate)
+        fmtstr['random_wincount'] = str(random_win)
+        fmtstr['random_losecount'] = str(random_lose)
+        fmtstr['random_stopcount'] = str(random_stop)
+        fmtstr['random_winrate'] = str(random_winrate)
 
         userinfo = UserInfoBuilder.instance().build(userdata, fmtstr)
         if userinfo is None:
--- user.md
+++ user.md
@@ -7,13 +7,13 @@
 **최고 RP:** {maxRatingPoint}
 
 **[공식전 전적]**
-**승리:** {records/$0/winCount}
-**패배:** {records/$0/loseCount}
-**탈주:** {records/$0/stopCount}
+**승리:** %rating_wincount
+**패배:** %rating_losecount
+**탈주:** %rating_stopcount
 **승률:** %rating_winrate
 
 **[일반전 전적]**
-**승리:** {records/$1/winCount}
-**패배:** {records/$1/loseCount}
-**탈주:** {records/$1/stopCount}
+**승리:** %random_wincount
+**패배:** %random_losecount
+**탈주:** %random_stopcount
 **승률:** %random_winrate
